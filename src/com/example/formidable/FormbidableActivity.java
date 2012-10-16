135774a2e8c226cf459db7f387caa9145bd42bc2
Chris Ford <christophertford@gmail.com>
2012-10-16 16:27:58 +0530

    Create events database (if it's not already there).

--- src/com/example/formidable/FormbidableActivity.java
+++ src/com/example/formidable/FormbidableActivity.java
@@ -2,10 +2,12 @@ package com.example.formidable;
 
 import java.io.IOException;
 
+import org.ektorp.CouchDbConnector;
 import org.ektorp.CouchDbInstance;
 import org.ektorp.http.HttpClient;
 import org.ektorp.impl.StdCouchDbInstance;
 
+import com.couchbase.touchdb.TDDatabase;
 import com.couchbase.touchdb.TDServer;
 import com.couchbase.touchdb.ektorp.TouchDBHttpClient;
 import com.couchbase.touchdb.router.TDURLStreamHandlerFactory;
@@ -17,16 +19,13 @@ import android.view.Menu;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
-
-
-
 public class FormbidableActivity extends Activity {
 	
 	static { 
 	    TDURLStreamHandlerFactory.registerSelfIgnoreError();
 	}
 	
-	private static final String TAG = "MyActivity";
+	private static final String TAG = "MainActivity";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,19 +36,17 @@ public class FormbidableActivity extends Activity {
         try {
             server = new TDServer(filesDir);
         } catch (IOException e) {
-            Log.e(TAG, "Error starting TDServer", e);
+            Log.e(TAG, "Error starting TouchDB Server.", e);
         }
         
-        HttpClient httpClient = new TouchDBHttpClient(server);
-        CouchDbInstance dbInstance = new StdCouchDbInstance(httpClient);
-        
+        CouchDbInstance client = new StdCouchDbInstance(new TouchDBHttpClient(server));  
+        CouchDbConnector events = client.createConnector("events", true);
         
         setContentView(R.layout.activity_main);
         WebView myWebView = (WebView) findViewById(R.id.webview);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         myWebView.loadUrl("http://enketo.org/launch?server=http%3A%2F%2Fformhub.org%2Fwho_forms");
-        //myWebView.loadUrl("http://m.google.com");
     }
 
     @Override

0d5b443d4f1baa561ad6bbad26454c9a721523eb
Pulkit <pulkit.bosco05@gmail.com>
2012-10-26 16:30:19 +0530

    pulkit | Adding a simple instruemntation-test project with a sample test

---
+++ instrumentation-test/src/com/example/formidable/FormidableTest.java
@@ -0,0 +1,10 @@
+package com.example.formidable;
+
+import android.test.InstrumentationTestCase;
+
+public class FormidableTest extends InstrumentationTestCase {
+
+    public void testSample() {
+        System.out.println("in test sample.");
+    }
+}

1909a61f0cd7c46962761896290567f1a9f7e670
Chris Ford <christophertford@gmail.com>
2012-10-31 14:37:47 +0530

    Made the results retrieval generic.

---
+++ formidable-client/src/com/example/formidable/RecordBuilder.java
@@ -0,0 +1,40 @@
+package com.example.formidable;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.codehaus.jackson.JsonNode;
+import org.ektorp.ViewResult;
+
+public class RecordBuilder {
+
+	private ViewResult.Row row;
+
+	public RecordBuilder(ViewResult.Row result) {
+		this.row = result;
+	}
+
+	public Map<String, Object> build() {	    
+        JsonNode data = row.getValueAsNode().get("data");
+        return map(data);
+	}
+	
+	private Map<String, Object> map(JsonNode node) {
+  
+        Map<String, Object> record = new HashMap<String, Object>();
+        
+        for(Iterator<Entry<String, JsonNode>> iter = node.getFields(); iter.hasNext(); ) {
+        		Entry<String, JsonNode> next = iter.next();
+        		if(next.getValue().getFields().hasNext()) {
+        			record.put(next.getKey(), map(next.getValue()));
+        		} else {
+        			record.put(next.getKey(), next.getValue().getTextValue());
+        		}
+        }
+        
+        return record;
+	}
+
+}
--- instrumentation-test/src/com/example/formidable/FormidableTest.java
+++ instrumentation-test/src/com/example/formidable/FormidableTest.java
@@ -1,13 +1,10 @@
 package com.example.formidable;
 
-import org.codehaus.jackson.JsonNode;
 import org.ektorp.ViewQuery;
 import org.ektorp.ViewResult;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.UUID;
 
 public class FormidableTest extends FormidableTestCase {
@@ -46,31 +43,11 @@ public class FormidableTest extends FormidableTestCase {
     }
 
     private Map<String, Object> getOnlyResult(ViewQuery viewQuery ) {
-    	
         ViewResult result = events.queryView(viewQuery);
-
-        ViewResult.Row record = result.getRows().get(0);     
-        JsonNode data = record.getValueAsNode().get("data");
-        JsonNode surname = data.get("surname");
-        JsonNode name = data.get("name");
-        
-        Map<String, Object> patient = new HashMap<String, Object>();
-        patient.put("name", name.getTextValue());
-        patient.put("surname", surname.getTextValue());
-        Map<String, Object> skills = new HashMap<String, Object>();
-        
-        if(data.get("skills") != null) {
-        		for (Iterator<Entry<String, JsonNode>> iter = data.get("skills").getFields(); iter.hasNext(); ) {
-        			Entry<String, JsonNode> entry = iter.next();
-        			skills.put(entry.getKey(), entry.getValue().getTextValue());
-        		}
-			patient.put("skills", skills);
-        }
-  
-        return patient;
+        return new RecordBuilder(result.getRows().get(0)).build();
     }
 
-    private void createEvent(int epoch, String recordId, String key, Object value) {
+	private void createEvent(int epoch, String recordId, String key, Object value) {
         Map<String, Object> map = new HashMap<String, Object>();
         map.put(key, value);
         Event event = new Event(epoch, recordId, map);

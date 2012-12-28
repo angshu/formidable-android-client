0f3f24204196c6f23e2f96de51579bc7d68f0476
angshu <angshuonline@gmail.com>
2012-12-28 16:50:58 +0530

    removed older form-files

--- formidable-client/assets/form-files/copyMnt.bat
+++
@@ -1,6 +0,0 @@
-rmdir /s /q F:\odk\js\forms\"
-xcopy . "F:\odk\js\forms" /E /I /R /Y
-del "F:\odk\js\forms\copy.bat"
-del "F:\odk\js\forms\copyMnt.bat"
-echo # %DATE% %TIME% >> "F:\odk\js\forms\default\manifest.appcache"
-# pause 'Hit enter to continue'
